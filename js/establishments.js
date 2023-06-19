0374ae6afc4b7594c2d717f606b37f8f00af3653
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-06-20 01:08:35 +0800

    add interactivity to establishments grid

--- js/establishments.js
+++ js/establishments.js
@@ -3,29 +3,34 @@ establishments = [{
     name: 'Mang Inasal',
     type1: 'Filipino',
     type2: 'Fastfood restaurant',
-    rating: '3.20'
+    rating: '3.20',
+    link: '#'
     }, {
     image: '../images/placeholder.jpg',
     name: 'Mang Inasal',
     type1: 'Filipino',
     type2: 'Fastfood restaurant',
-    rating: '3.20'  
+    rating: '3.20',
+    link: '#'
     }, {
     image: '../images/placeholder.jpg',
     name: 'Mang Inasal',
     type1: 'Filipino',
     type2: 'Fastfood restaurant',
-    rating: '3.20'  
+    rating: '3.20',
+    link: '#'
     }, {
     image: '../images/placeholder.jpg',
     name: 'Mang Inasal',
     type1: 'Filipino',
     type2: 'Fastfood restaurant',
-    rating: '3.20'  
+    rating: '3.20',
+    link: '#'
     }, {
     image: '../images/placeholder.jpg',
     name: 'Mang Inasal',
     type1: 'Filipino',
     type2: 'Fastfood restaurant',
-    rating: '3.20'  
+    rating: '3.20',
+    link: '#'
     }];
