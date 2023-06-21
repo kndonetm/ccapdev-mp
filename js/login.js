fefd0d2655d91be9934d1e68cf2ba3bba6d2aa32
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-06-22 03:21:12 +0800

    use localstorage api for login status

--- js/login.js
+++ js/login.js
@@ -1,4 +1,9 @@
-let currentLogin = false;
+// Check if the user is already logged in
+let currentLogin = localStorage.getItem('currentLogin') === 'true';
+let savedUsername = localStorage.getItem('username');
+
+// Update the navbar based on the stored login status and username
+updateNavbar();
 
 document.querySelector('.signin-js').addEventListener('click', () => {
     login(document.querySelector('#username-login').value, document.querySelector('.signin-modal'));
@@ -13,24 +18,52 @@ document.querySelector('.reg-js').addEventListener('click', () => {
 
 function login(username, modal, image = 'icon.png') {
     if (!currentLogin) {
-        login = true;
+        currentLogin = true;
+        savedUsername = username;
 
-        let navHTML = `
-        <li class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle p-0" role="button" data-bs-toggle="dropdown">
-                <div class="d-inline-block mb-0">
-                    <img src="../images/${image}" alt="" class="pfp">
-                    <span class="fs-6">Hello,<span class="fw-semibold"> ${username}</span> </span>
-                </div>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="" class="dropdown-item">Profile</a></li>
-                <li><a href="" class="dropdown-item">Logout</a></li>
-            </ul>
-        </li>`;
+        // Store the login status and username in localStorage
+        localStorage.setItem('currentLogin', currentLogin);
+        localStorage.setItem('username', savedUsername);
+
+        updateNavbar();
 
         $(modal).modal('hide');
-
-        document.querySelector('.navbar-nav').innerHTML = navHTML;
     }
+}
+
+function updateNavbar() {
+    let navHTML = '';
+
+    if (currentLogin) {
+        navHTML = `
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle p-0" role="button" data-bs-toggle="dropdown">
+                    <div class="d-inline-block mb-0">
+                        <img src="../images/icon.png" alt="" class="pfp">
+                        <span class="fs-6">Hello,<span class="fw-semibold"> ${savedUsername}</span> </span>
+                    </div>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="" class="dropdown-item profile-view">Profile</a></li>
+                    <li><a href="" class="dropdown-item logout">Logout</a></li>
+                </ul>
+            </li>`;
+    } else {
+        // Default navbar HTML when the user is not logged in
+        navHTML = `
+            <li class="nav-item me-2">
+            <button class="btn btn-success rounded-5" data-bs-target="#signin" data-bs-toggle="modal">Sign in</button>
+            </li>
+            <li class="nav-item">
+                <button class="btn btn-success rounded-5" data-bs-target="#register" data-bs-toggle="modal">Sign up</button>
+            </li>`;
+    }
+
+    document.querySelector('.navbar-nav').innerHTML = navHTML;
+}
+
+if (currentLogin) {
+    document.querySelector('.logout').addEventListener('click', () => {
+        localStorage.clear();
+    })
 }
