09afd8bf7b1847db6fada145419342e42ac96826
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-06-22 02:26:37 +0800

    change navbar when registered

--- js/login.js
+++ js/login.js
@@ -1,18 +1,25 @@
-let login = false;
+let currentLogin = false;
 
+document.querySelector('.signin-js').addEventListener('click', () => {
+    login(document.querySelector('#username-login').value, document.querySelector('.signin-modal'));
+})
 
+document.querySelector('.reg-js').addEventListener('click', () => {
+    const fileInput = document.querySelector('#file');
+    const file = fileInput.files[0];
+    file ? login(document.querySelector('#username-reg').value, document.querySelector('.reg-modal'), file.name) :
+           login(document.querySelector('#username-reg').value, document.querySelector('.reg-modal'));
+})
 
-if (!login) {
-    document.querySelector('.signin-js').addEventListener('click', () => {
+function login(username, modal, image = 'icon.png') {
+    if (!currentLogin) {
         login = true;
-        let username = document.querySelector('#username-login').value;
-        let password = document.querySelector('#password-login').value;
-    
+
         let navHTML = `
         <li class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle p-0" role="button" data-bs-toggle="dropdown">
                 <div class="d-inline-block mb-0">
-                    <img src="../images/icon.png" alt="">
+                    <img src="../images/${image}" alt="" class="pfp">
                     <span class="fs-6">Hello,<span class="fw-semibold"> ${username}</span> </span>
                 </div>
             </a>
@@ -21,12 +28,9 @@ if (!login) {
                 <li><a href="" class="dropdown-item">Logout</a></li>
             </ul>
         </li>`;
-    
-        let modal = document.querySelector('.signin-modal');
-        $(modal).modal('hide');
-    
-        document.querySelector('.navbar-nav').innerHTML = navHTML;
-    })
-    
-}
 
+        $(modal).modal('hide');
+
+        document.querySelector('.navbar-nav').innerHTML = navHTML;
+    }
+}
