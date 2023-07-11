2abdbf1d22980d9a8bbf28f6a7e24cce1d8a0247
kndone <justinlim4916@gmail.com>
2023-07-11 23:55:45 +0800

    Added routes and mongodb connection

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-express = require('express');
-exphbs = require('express-handlebars');
+import "dotenv/config";
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
 
-const port = 3000;
+import express from 'express';
+import exphbs from 'express-handlebars';
+
+import router from './src/routes/index-router.js';
+
+import { connectToMongo } from './src/model/conn.js';
 
 const app = express();
 
-app.use("/static", express.static("public"));
+const __dirname = dirname(fileURLToPath(import.meta.url)); // directory URL
+app.use("/static", express.static(__dirname + "/public"));
 
 app.engine("hbs", exphbs.engine({ 
     extname: "hbs",
 }));
 app.set("view engine", "hbs");
-app.set("views", "./views");
+app.set("views", "./src/views");
+app.set("view cache", false);
 
-app.get("/", function(req, res) {
-    res.render("index", {
-        title: "Home"
-    })
-})
+app.use(router);
 
-
-
-
-app.listen(port, function() {
-    console.log("Console now listening on port " + port);
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Express app now listening...");
+    connectToMongo((err) => {
+        if (err) {
+            console.error("An error has occurred:");
+            console.error(err);
+            return;
+        }
+        console.log("Connected to Mongodb");
+    });
 });
-
---
+++ src/routes/index-router.js
@@ -0,0 +1,48 @@
+import { Router } from 'express'
+
+const router = Router();
+
+router.get("/", function(req, res) {
+    res.render("index", {
+        title: "Home"
+    });
+})
+
+router.get("/search", function(req, res) {
+    res.render("search-result-view", {
+        title: "Search results"
+    })
+})
+
+router.get("/users/:userid", function(req, res) {
+    console.log(req.params.userid);
+    res.render("user-profile-view", {
+        title: "User profile"
+    });
+})
+
+router.get("/:establishmentid", function(req, res) {
+    console.log(req.params.establishmentid);
+    res.render("establishment-view", {
+        title: "Establishment view"
+    })
+})
+
+router.post('/login', function(req, res) {
+    res.redirect("/");
+})
+
+router.post('/register', function(req, res) {
+    res.redirect("/");
+})
+
+router.post('/comment', function(req, res) {
+    res.redirect("/");
+})
+
+router.post('/:establishmentid', function(req, res) {
+    console.log(req.params.establishmentid);
+    res.redirect("/");
+})
+
+export default router;
