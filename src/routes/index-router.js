32c59cb76f96afeb5b96a0893719af092752b188
skeeDayo <steven_escopete@dlsu.edu.ph>
2023-07-12 20:26:26 +0800

    update main.hbs head

--- src/routes/index-router.js
+++ src/routes/index-router.js
@@ -43,3 +43,21 @@ router.post('/:establishmentid', function(req, res) {
 })
 
 export default router;
+
+/*
+index
+css:'<link rel="stylesheet" href="/static/css/index.css">',
+js: '<script src="static/js/card-view.js" defer></script>'
+
+estab
+css:'<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">',
+js: '<script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>'
+
+user
+css:'<link href="/static/css/user-profile.css" rel="stylesheet">',
+js: '<script defer src="/static/js/user-profile.js"></script>
+
+search
+css:'<link href="/static/css/search-result.css" rel="stylesheet">',
+js :'<script defer src="/static/js/search-result.js"></script>'
+*/
--- src/routes/search-router.js
+++ src/routes/search-router.js
@@ -5,13 +5,8 @@ const searchRouter = Router();
 searchRouter.get("/search", (req, res) => {
     res.render("search", {
         title: req.query.q + " - Search Results",
-        css: '<link href="static/css/stylesheet.css" rel="stylesheet">' +
-             '<link href="static/css/search-result.css" rel="stylesheet">',
-        js: '<script src="static/js/attach-pfp.js" defer></script>' +
-            '<script src="static/js/navbar.js" defer></script>' +
-            '<script defer src="../js/search-result.js"></script>' +
-            '<script src="static/js/authentication.js" defer></script>' +
-            '<script defer src="../js/establishments.js"></script>',
+        css:'<link href="static/css/search-result.css" rel="stylesheet">',
+        js: '<script defer src="../js/search-result.js"></script>',
         key: req.query.q
     })
 });
