247f6f6cf995d3581ed9af50bf5fdb6b50c917a6
skeeDayo <steven_escopete@dlsu.edu.ph>
2023-07-14 21:18:37 +0800

    implement search

--- src/routes/search-router.js
+++ src/routes/search-router.js
@@ -1,13 +1,49 @@
 import { Router } from 'express';
+import { getDb } from '../model/conn.js';
 
 const searchRouter = Router();
+const db = getDb();
+const establishments = db.collection("establishments");
+const reviews = db.collection("reviews");
 
-searchRouter.get("/search", (req, res) => {
+searchRouter.get("/search", async (req, res) => {
+    const estabQueryPipe = {
+        $or: [
+            { "displayed-name": { $regex: new RegExp(req.query.q, "i") } },
+            { description: { $regex: new RegExp(req.query.q, "i") } }
+        ]
+    };
+
+    const reviewQueryPipe = [
+        {
+            $match: {
+                $or: [
+                    { title: { $regex: new RegExp(req.query.q, "i") } },
+                    { content: { $regex: new RegExp(req.query.q, "i") } }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "user-id",
+                foreignField: "_id",
+                as: "user"
+            }
+        },
+        { $unwind: "$user" }
+    ];
+
+    const establishmentsArray = await establishments.find(estabQueryPipe).toArray();
+    const reviewsArray = await reviews.aggregate(reviewQueryPipe).toArray();
+
     res.render("search", {
         title: req.query.q + " - Search Results",
         css:'<link href="static/css/search-result.css" rel="stylesheet">',
         js: '<script defer src="../js/search-result.js"></script>',
-        key: req.query.q
+        key: req.query.q,
+        establishments: establishmentsArray,
+        reviews: reviewsArray
     })
 });
 
