6c579e93d5cdaa1a5887782304b31c8b07744699
skeeDayo <steven_escopete@dlsu.edu.ph>
2023-07-16 18:32:45 +0800

    fixed filter

--- src/routes/search-router.js
+++ src/routes/search-router.js
@@ -14,6 +14,11 @@ searchRouter.get("/search", async (req, res) => {
         ]
     };
 
+    if(req.query.filter) {
+        let ratingFloor = Math.floor(req.query.filter);
+        estabQueryPipe.$and = [ { rating: { $gt: ratingFloor, $lt: ratingFloor + 1 } } ]
+    }
+
     const reviewQueryPipe = [
         {
             $match: {
@@ -40,7 +45,7 @@ searchRouter.get("/search", async (req, res) => {
     res.render("search", {
         title: req.query.q + " - Search Results",
         css:'<link href="static/css/search-result.css" rel="stylesheet">',
-        js: '<script defer src="../js/search-result.js"></script>',
+        js: '<script defer src="static/js/search-result.js"></script>',
         key: req.query.q,
         establishments: establishmentsArray,
         reviews: reviewsArray
